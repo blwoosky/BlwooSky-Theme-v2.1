d9797c2823fd975465910e539429c6a1a553c1a5
BlwooSky <blwoosky>
2016-07-11 06:00:45 +0800

    update config

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,22 @@
 import React, { Component,Children } from 'react';
-
+//import Header from './Header';
 
 export default class Main extends Component {
+
     constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <div className="mt30">
-                <div className="wrap">
-                    <div className="mainWrap fix">
-                        <div className="l col-2-3">
-                            <div>
-                                <h1>Hello ,Welcome TO React Wordfdfld!</h1>
-                                <div className="loadingContent">
-                                    {this.props.children}
-                                </div>
-                            </div>
-                        </div>
+            <div className="w">
+                <div className="pl20 pr20">
+                    <div className="loadingContent">
+                        {this.props.children}
                     </div>
                 </div>
             </div>
+
         );
     }
 }
